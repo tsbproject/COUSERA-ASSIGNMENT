8d477d5b6656e54c82206c2fcc8a5d690b581bf6
TSB <122724396+tsbproject@users.noreply.github.com>
2023-02-13 22:36:42 +0100

    Update SpeakHello.js

--- Module4-Solution/SpeakHello.js
+++ Module4-Solution/SpeakHello.js
@@ -37,6 +37,7 @@ function speak(name) {
 (function(name){
   var helloSpeaker = {};
   var speakWord = "Hello";
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   helloSpeaker.speak = function (name){
      console.log(speakWord +' '+ name);
   }
